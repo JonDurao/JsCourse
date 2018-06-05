c5d92392e007317f7263cf79a8193fc7de7f2c95
Jon Durao <jon.durao@gmail.com>
2018-06-05 17:43:19 +0200

    Update

--- Section 8/1 - Async/script.js
+++ Section 8/1 - Async/script.js
@@ -55,4 +55,123 @@ function getRecipe() {
 }
 
 getRecipe();*/
-// Lecture 114 (Async with Promises "The ES6 way")
+/**
+ * Lecture 114 (Async with Promises "The ES6 way")
+ *
+ * Keeps track whether a certain event has happened
+ * Determines what happened after the event
+ * Implements concept of future value that we're expecting
+ *
+ * STATES => PENDING || SETTLED OR RESOLVED || (FULFILLED || REJECTED)
+ *
+ * We can produce and consume promises
+
+
+// function executor immediately called when created
+const getIDs = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const val = Math.floor(Math.random() * Math.floor(2));
+        if (val === 1)
+            resolve([1,2,3,4,5,6,7]);
+        else
+            reject();
+    }, 3000);
+});
+
+// THEN => when successful
+// CATCH => when error
+// FINALLY => Always at the end
+getIDs.then(() => console.log('SUCCESS!'))
+    .catch(() => console.log('ERROR!'))
+    .finally(() => console.log('THE END'));
+
+const getIDs = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve([1,2,3,4,5,6,7]);
+    }, 3000);
+});
+
+const getRecipe = recID => {
+    return new Promise((resolve, reject) => {
+        setTimeout(id => {
+            const recipe = {title: 'Pasta', author: 'Jon'};
+            resolve(recipe);
+        }, 3000, recID);
+    });
+};
+
+const getRelated = publisher => {
+    return new Promise((resolve, reject) => setTimeout(pub => {
+        const recipe2 = {title: 'Pineapple pizza', author: 'JonTwo'};
+        reject(`${recipe2.title}, ${pub}`);
+    }, 1500, publisher));
+};
+
+// One catch works for every promise
+getIDs.then(IDs => {
+    console.log(IDs);
+    return getRecipe(IDs[4]);
+})
+    .then(recipe => {
+        console.log(recipe);
+        return getRelated(recipe.author);
+    })
+        .then(recipe => {
+            console.log(recipe);
+        })
+.catch(recipe => console.log(`${recipe}, is a mistake`));*/
+/**
+ * Lecture 115 (Async/Await ES8)
+
+const getIDs = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve([1,2,3,4,5,6,7]);
+    }, 3000);
+});
+
+const getRecipe = recID => {
+    return new Promise((resolve, reject) => {
+        setTimeout(id => {
+            const recipe = {title: 'Pasta', author: 'Jon'};
+            resolve(recipe);
+        }, 3000, recID);
+    });
+};
+
+const getRelated = publisher => {
+    return new Promise((resolve, reject) => setTimeout(pub => {
+        const recipe2 = {title: 'Pineapple pizza', author: 'JonTwo'};
+        resolve(`${recipe2.title}, ${pub}`);
+    }, 1500, publisher));
+};
+
+// async functions always run in the background
+// await stops the function and wait for the event in the promise to return
+// assigns the return value of the recipe to the variable
+async function getRecipesAW() {
+    const IDs = await getIDs;
+    console.log(IDs);
+    const recipe = await getRecipe(IDs[4]);
+    console.log(recipe);
+    const related = await getRelated(recipe.author);
+    console.log(related);
+
+    return related;
+}
+
+// logs the promise as pending
+// const rec = getRecipesAW();
+// console.log(rec);
+
+getRecipesAW().then(result => console.log(result + ', Is the Best'));*/
+/**
+ * Lecture 116 (AJAX - Async Js and Xml - and APIs - App Programming Interface -)
+ * AJAX allows to communicate with a server in an async way
+ * JS app => server without reloading whole page
+ * AJAX makes a HTTP GET request async in the background
+ * Also can send data to the server
+ *
+ * API allow apps to talk to each other
+ * It's part of the server
+ * Own APIs or External APIs (GMaps, Weather, Movies...)
+ */
